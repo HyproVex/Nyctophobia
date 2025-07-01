0b849f060c353de72d0790d11a4c30d555407a03
HyproVex <157707857+HyproVex@users.noreply.github.com>
2025-07-01 18:52:41 +0200

    Version 0.0.0 Aplha
    - Sector spliting and their placement
    - Button function testing

--- main_function.js
+++ main_function.js
@@ -1,2 +1,8 @@
-window.alert("Version 0.0.0 Pre-Alpha")
+window.alert("Version 0.0.0 Alpha")
 window.alert("Feel free to report bugs or make recommendations")
+
+
+document.getElementById("test").onclick = function(){
+    document.getElementById("test_text").textContent = "Lighted";
+    document.getElementById("text1").textContent = "You lighted a match."
+}
