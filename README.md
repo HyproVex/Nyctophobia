199dbfb600e8f93a8e0a3fb2898f2e94dd8355b3
HyproVex <157707857+HyproVex@users.noreply.github.com>
2025-07-02 22:27:06 +0200

    V. 0.0.0 Beta
    - (very) Light storytelling
    
    - New font ( Courier New, Courier, monospace )
    
    - padding for LOG, MAIN, OTHERS
    
    - Match counter by pressing button

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Nyctophobia
+
 Incremental Game 1
 
 Game made by HyproVex
 
 My first attempt to make ASCII/Incremental browser game.
+Also by making this game I'm also learning in HTML,CSS,JS so why not :D
 
 site: https://hyprovex.github.io/Nyctophobia/
+
+
+# Story Summary 
+
+You woke up in a small room, no bigger than a toilet room. Your memories are blurry. The darkness is making you anxious.
--- main_function.js
+++ main_function.js
@@ -1,8 +1,32 @@
-window.alert("Version 0.0.0 Alpha")
-window.alert("Feel free to report bugs or make recommendations")
+
+// Messages for player
+
+window.alert("Version 0.0.0 Beta");
+window.alert("Feel free to report bugs or make recommendations");
 
 
-document.getElementById("test").onclick = function(){
+// Inicializing for match counting
+
+const Matches = document.getElementById("MatchCounter");
+const MatchButton = document.getElementById("MatchButton");
+let CounterMatches = 0;
+
+
+// first version of updating text by button
+
+/* 
+document.getElementById("MatchButton").onclick = function(){
     document.getElementById("test_text").textContent = "Lighted";
-    document.getElementById("text1").textContent = "You lighted a match."
+    document.getElementById("text1").textContent = "You lighted a match.";
+}
+*/
+
+
+// Counter by clicking button ( Matches )
+
+MatchButton.onclick = function(){
+    document.getElementById("test_text").textContent = "Lighted matches";
+    document.getElementById("text1").textContent = "You lighted a match. The room is smaller than you expected. And yet. It's still too dark."
+    CounterMatches += 1;
+    Matches.textContent = CounterMatches;
 }
