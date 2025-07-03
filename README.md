5841e9b498cdf2afa7040ac06a9b57efb3edc40d
HyproVex <157707857+HyproVex@users.noreply.github.com>
2025-07-03 20:46:33 +0200

    V. 0.0.0 Beta-Build[2]
    - Testing milestones ( Javascript Improvement)
    - Current version in the left corner
    - Link for GitHub
    - Improved light button
    - reversed button and "Lighted matches"

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Incremental Game 1
 Game made by HyproVex
 
 My first attempt to make ASCII/Incremental browser game.
-Also by making this game I'm also learning in HTML,CSS,JS so why not :D
 
 site: https://hyprovex.github.io/Nyctophobia/
 
@@ -13,3 +12,17 @@ site: https://hyprovex.github.io/Nyctophobia/
 # Story Summary 
 
 You woke up in a small room, no bigger than a toilet room. Your memories are blurry. The darkness is making you anxious.
+
+
+# ChangeLog
+
+Xx ---------- 0.0.0 Beta-Build[2] ---------- xX
+
+<< ADDED >>
+
+- Testing milestones ( Javascript Improvement)
+- Current version in the left corner 
+- Link for GitHub 
+- Improved light button 
+- reversed button and "Lighted matches"
+
--- main_function.js
+++ main_function.js
@@ -1,7 +1,6 @@
 
 // Messages for player
 
-window.alert("Version 0.0.0 Beta");
 window.alert("Feel free to report bugs or make recommendations");
 
 
@@ -12,21 +11,43 @@ const MatchButton = document.getElementById("MatchButton");
 let CounterMatches = 0;
 
 
-// first version of updating text by button
+// LOG text
 
-/* 
-document.getElementById("MatchButton").onclick = function(){
-    document.getElementById("test_text").textContent = "Lighted";
-    document.getElementById("text1").textContent = "You lighted a match.";
-}
-*/
+let text1 = document.getElementById("text1");
+let text2 = document.getElementById("text2");
+let text3 = document.getElementById("text3");
+let text4 = document.getElementById("text4");
+let text5 = document.getElementById("text5");
 
 
 // Counter by clicking button ( Matches )
 
 MatchButton.onclick = function(){
     document.getElementById("test_text").textContent = "Lighted matches";
-    document.getElementById("text1").textContent = "You lighted a match. The room is smaller than you expected. And yet. It's still too dark."
     CounterMatches += 1;
     Matches.textContent = CounterMatches;
+
+    if(CounterMatches == 1){
+        text1.textContent = "You lighted a match. The room is smaller than you expected. And yet. It's still too dark.";    
+    }
+
+    if(CounterMatches >= 50){
+        text1.textContent = "you made 50 lighted matches";
+        text2.textContent = "You lighted a match. The room is smaller than you expected. And yet. It's still too dark.";
+    }
+
+    if(CounterMatches >= 100){
+        text1.textContent = "why did you made 100 lighted matches?";    
+        text2.textContent = "you made 50 lighted matches";
+        text3.textContent = "You lighted a match. The room is smaller than you expected. And yet. It's still too dark.";
+    }
+
+    if(CounterMatches >= 200){
+        text1.textContent = "stop it";        
+        text2.textContent = "why did you made 100 lighted matches?";
+        text3.textContent = "you made 50 lighted matches";
+        text4.textContent = "You lighted a match. The room is smaller than you expected. And yet. It's still too dark.";
+    }
 }
+
+
